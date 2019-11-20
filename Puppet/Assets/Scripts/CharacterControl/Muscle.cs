acfc72b98f69cbac7432f8d1d862489b0624a07b
Pistoncito <victorglezrivera@hotmail.com>
2019-11-20 22:47:15 +0100

    puppet medio funcional

---
+++ Puppet/Assets/Scripts/CharacterControl/Muscle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Muscle
+{
+    public Rigidbody2D bone;
+    public float restingRot;
+    public float restForce;
+    public float flexedRot;
+    public float flexForce;
+    public bool isFlexed;
+
+    public void ActiveMuscle()
+    {
+        bone.MoveRotation(Mathf.LerpAngle(bone.rotation, restingRot, restForce * Time.deltaTime));
+        /*
+        if (!isFlexed)
+        {
+            bone.MoveRotation(Mathf.LerpAngle(bone.rotation, restingRot, restForce * Time.deltaTime));
+        }
+        else
+        {
+            bone.MoveRotation(Mathf.LerpAngle(bone.rotation, flexedRot, flexForce * Time.deltaTime));
+        }
+        */
+    }
+
+
+}
---
+++ Puppet/Assets/Scripts/CharacterControl/Puppet.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum BodyParts
+{
+    CABEZA,
+    TORSO,
+    BRAZO_IZ,
+    BRAZO_DER,
+    MANO_IZ,
+    MANO_DER,
+    PIERNA_IZ,
+    PIERNA_DER,
+    PIE_IZ,
+    PIE_DER
+}
+
+public class Puppet : MonoBehaviour
+{
+    GameObject[] bodyParts;
+    public Muscle[] muscles;
+    private Vector2[] initLocalPositions;
+    private bool isGrounded = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        SetBodyParts();
+        SetLocalPositions();
+        //StartCoroutine(CheckGrounded());
+        //DisableCollisions();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActiveMuscles();
+    }
+
+    void ActiveMuscles()
+    {
+        foreach (Muscle m in muscles)
+        {
+            m.ActiveMuscle();
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            //ArmsFlex();
+        }
+        else
+        {
+            //RelaxMuscles();
+        }
+    }
+    void ArmsFlex()
+    {
+        muscles[(int)BodyParts.BRAZO_DER].isFlexed = true;
+        muscles[(int)BodyParts.BRAZO_IZ].isFlexed = true;
+        muscles[(int)BodyParts.MANO_DER].isFlexed = true;
+        muscles[(int)BodyParts.MANO_IZ].isFlexed = true;
+    }
+
+    void RelaxMuscles()
+    {
+        foreach(Muscle m in muscles)
+        {
+            m.isFlexed = false;
+        }
+    }
+    void SetLocalPositions()
+    {
+        initLocalPositions = new Vector2[bodyParts.Length];
+        for(int i = 0; i< initLocalPositions.Length; i++)
+        {
+            initLocalPositions[i] = bodyParts[i].transform.localPosition;
+        }
+    }
+
+    void Flex(BodyParts bp, Vector2 direction, float force)
+    {
+        Rigidbody2D bone = bodyParts[(int)(bp)].transform.GetComponent<Rigidbody2D>();
+        bone.AddForce(force * direction);
+        
+    }
+
+    void SetBodyParts()
+    {
+        bodyParts = new GameObject[System.Enum.GetNames(typeof(BodyParts)).Length];
+        for (int i = 0; i< bodyParts.Length; i++)
+        {
+            bodyParts[i] = this.transform.GetChild(i).gameObject;
+        }
+    
+    }
+
+    IEnumerator CheckGrounded()
+    {
+        Collider2D floor = GameObject.Find("floor").transform.GetComponent<Collider2D>();
+        Collider2D leftFoot = bodyParts[(int)BodyParts.PIERNA_IZ].transform.GetComponent<Collider2D>();
+        Collider2D rightFoot = bodyParts[(int)BodyParts.PIERNA_IZ].transform.GetComponent<Collider2D>();
+
+     ;
+        do
+        {
+            if (leftFoot.IsTouching(floor) && leftFoot.IsTouching(floor))
+            {
+                isGrounded = true;
+            }else
+            {
+                isGrounded = false;
+                //poner bien
+                Debug.Log("Poner bien el puppet");
+                
+            }
+            yield return new WaitForSeconds(2.0f);
+        } while (true);
+    }
+    void DisableCollisions()
+    {
+        for (int i = 0; i < bodyParts.Length; i++)
+        {
+            Collider2D target = bodyParts[i].GetComponent<Collider2D>();
+            for (int j = 0; j < bodyParts.Length; j++)
+            {
+                Collider2D current = bodyParts[j].GetComponent<Collider2D>();
+                Physics2D.IgnoreCollision(target, current);
+            }
+                
+        }
+    }
+
+}
