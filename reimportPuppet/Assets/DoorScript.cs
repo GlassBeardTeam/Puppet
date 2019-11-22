60f7c3b7dd6f3d1fa083a611a5465627c2b2b78f
Adrián Poza <adripoza23@gmail.com>
2019-11-22 23:11:44 +0100

    puerta

---
+++ reimportPuppet/Assets/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Stack<GameObject> gears = new Stack<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            gears.Push(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showGear()
+    {
+        gears.Pop().SetActive(true);
+    }
+}
---
+++ reimportPuppet/Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    int gears;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gears = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addGear()
+    {
+        gears++;
+        Debug.Log("+1 punto");
+        //TODO update HUD
+        GameObject.Find("door").GetComponent<DoorScript>().showGear();
+    }
+}
