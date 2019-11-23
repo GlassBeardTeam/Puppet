e775d21841fce91cf33c734c8da093ef3f881d1e
adrian <adripoza23@gmail.com>
2019-11-23 14:34:31 +0100

    cronometro mal hecho

--- reimportPuppet/Assets/GameController.cs
+++ reimportPuppet/Assets/GameController.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
 
-    public Text gearsText;
     public Text coilText;
+    public Text timerText;
 
     int gears;
     int coil;
+
+    int minutes = 0;
+    int seconds = 0;
+    float tenths = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +26,23 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (tenths > 9)
+        {
+            seconds++;
+            if(seconds > 59)
+            {
+                minutes++;
+            }
+        }
+
+        tenths += Time.deltaTime * 10;
+
+        timerText.text = "" + minutes + " : " + seconds + " : " + (int)tenths;
     }
 
     public void addGear()
     {
         gears++;
-        gearsText.text = gears.ToString();
         GameObject.Find("door").GetComponent<DoorScript>().showGear();
     }
 
