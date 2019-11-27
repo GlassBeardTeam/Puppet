6449735d072a828beae58a8e38ccfe9df8380c2f
Adrián Poza <adripoza23@gmail.com>
2019-11-27 19:43:17 +0100

    hud bonico

--- Pappet/Assets/Scripts/Camera/CameraFollow.cs
+++ Pappet/Assets/Scripts/Camera/CameraFollow.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float speed = 0.1f;
-    public Vector3 offset = new Vector3(0, 0, -10);
+    public float speed = 1.0f;
+
+    Rigidbody2D rg;
 
     private void Start()
     {
-        Debug.Log(transform.position);
+        rg = GetComponent<Rigidbody2D>();
     }
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 nextPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
-        transform.position = nextPosition;
+        rg.velocity = (target.GetChild(0).GetChild(0).position - transform.position)*speed;
     }
 
 }
