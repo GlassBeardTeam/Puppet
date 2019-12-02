71df255a84446769dbfb36fa0d62af8cae0ccbaf
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2019-12-02 18:21:02 +0100

    Nivel Terminado

--- Pappet/Assets/Scripts/SpringPlatform.cs
+++ Pappet/Assets/Scripts/SpringPlatform.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class SpringPlatform : MonoBehaviour
 {
+    GameObject bouncer;
+
+    public float multiplier;
+    public bool customJump;
+    public Vector2 customVelocity;
+
+    Vector2 velocity;
+    Animator anim;
     Rigidbody2D rb;
+
+    bool onTop;
     float initHeight;
     float yoffset = 0.1f;
     float upwardForce;
@@ -12,6 +22,7 @@ public class SpringPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         initHeight = transform.position.y;
     }
@@ -19,6 +30,7 @@ public class SpringPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         upwardForce = Mathf.Clamp(Mathf.Pow(((initHeight - transform.position.y)*2), 10), 0, 10000);
         rb.AddForce(new Vector2(0, upwardForce));
         if(transform.position.y > initHeight)
@@ -31,6 +43,7 @@ public class SpringPlatform : MonoBehaviour
             upwardForce = Mathf.Clamp(Mathf.Pow(((initHeight - transform.position.y) * 2), 10), 0, 30000);
         }
         rb.AddForce(new Vector2(0, upwardForce));
+        */
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -39,7 +52,43 @@ public class SpringPlatform : MonoBehaviour
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        rb.AddForce(new Vector2(0, 2000));
+        //rb.AddForce(new Vector2(0, 2000));
+        if (onTop)
+        {
+            anim.SetBool("isStepped", true);
+            if(collision.gameObject.tag == "PuppetPart")
+            {
+                bouncer = Puppet.Instace_.bodyParts[(int)BodyParts.TORSO];
+            } else
+            {
+                bouncer = collision.gameObject;
+            }
+            
+        }       
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        onTop = true;
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onTop = false;
+        anim.SetBool("isStepped", false);
+    }
+
+    private void Jump()
+    {
+        if (customJump)
+        {
+            velocity = customVelocity;
+        } else
+        {
+            velocity = transform.up * multiplier;
+        }
+
+        Rigidbody2D bouncerBody = bouncer.GetComponent<Rigidbody2D>();
+        bouncerBody.velocity = velocity;
     }
 }
