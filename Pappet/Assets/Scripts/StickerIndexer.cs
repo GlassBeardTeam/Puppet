bd57c955bf950e39110331b6556e039a97ee3d8c
Adrián Poza <adripoza23@gmail.com>
2019-12-04 19:21:39 +0100

    customizacion seleccion pegatinas

---
+++ Pappet/Assets/Scripts/StickerIndexer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickerIndexer : MonoBehaviour
+{
+    MenuController menuController;
+
+    public int SetIndex;
+    public int PartIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuController = GameObject.FindGameObjectWithTag("CustomizationController").GetComponent<MenuController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void selectSticker()
+    {
+        menuController.selectSticker(SetIndex, PartIndex);
+    }
+}
